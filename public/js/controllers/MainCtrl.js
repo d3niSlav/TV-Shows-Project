38e9d4f0e00dea244831d752a51f65646f9a1da1
Denislav Enchev <deni.b.enchev@gmail.com>
2017-04-21 19:34:16 +0300

    Mobile and tablet view for the header and the tv show details page added.

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,1 +1,7 @@
-angular.module('MainCtrl', []).controller('MainController', function() {});
+angular.module('MainCtrl', []).controller('MainController', ['$scope', function($scope) {
+    $scope.mobileButtonClick = function() {
+        $('#mobile-nav-btn').toggleClass("is-active");
+        $('#mobile-logo').toggleClass("hide");
+        $('#mobile-menu').toggleClass("hide");
+    }
+}]);
