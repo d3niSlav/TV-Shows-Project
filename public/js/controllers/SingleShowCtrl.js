ff7a480a7071ff447fa8b6c3dc57064a453da361
Denislav Enchev <deni.b.enchev@gmail.com>
2017-04-26 17:25:33 +0300

    Single show navigation added.

--- public/js/controllers/SingleShowCtrl.js
+++ public/js/controllers/SingleShowCtrl.js
@@ -1,12 +1,13 @@
 angular.module('SingleShowCtrl', []).controller('SingleShowController', ['$scope', '$routeParams', 'SingleShow', function($scope, $routeParams, SingleShow) {
-    $scope.showEpisodesClass = "";
-    $scope.hideEpisodesClass = "hide";
 
     SingleShow.getShowData($routeParams.showId)
         .then(function(res) {
             $scope.show = res.data;
             $scope.currentSeason = res.data.seasons[res.data.seasons.length - 1];
         });
+
+    $scope.showEpisodesClass = "";
+    $scope.hideEpisodesClass = "hide";
 
     $scope.changeSeason = function(index) {
         $scope.currentSeason = $scope.show.seasons[index];
@@ -17,10 +18,47 @@ angular.module('SingleShowCtrl', []).controller('SingleShowController', ['$scope
     $scope.showEpisodes = function() {
         $scope.showEpisodesClass = "hide";
         $scope.hideEpisodesClass = "";
+
+        $('html,body').animate({ scrollTop: $("#show-episodes").offset().top }, 'slow');
     }
 
     $scope.hideEpisodes = function() {
         $scope.showEpisodesClass = "";
         $scope.hideEpisodesClass = "hide";
+    }
+
+    $scope.navigateToSeasons = function() {
+        $('#show-seasons').show();
+        $('#show-seasons').siblings().hide();
+        $('#goToSeasonsInfo').addClass('active');
+        $('#goToComments').removeClass('active');
+        $('#goToTrailer').removeClass('active');
+        $('html,body').animate({ scrollTop: $("#show-seasons").offset().top }, 'slow');
+    }
+
+    $scope.navigateToComments = function() {
+        $('#show-comments').show();
+        $('#show-comments').siblings().hide();
+        $('#goToComments').addClass('active');
+        $('#goToSeasonsInfo').removeClass('active');
+        $('#goToTrailer').removeClass('active');
+        $('html,body').animate({ scrollTop: $("#show-comments").offset().top }, 'slow');
+    }
+
+    $scope.navigateToTrailer = function() {
+        $('#show-trailer').show();
+        $('#show-trailer').siblings().hide();
+        $('#goToTrailer').addClass('active');
+        $('#goToComments').removeClass('active');
+        $('#goToSeasonsInfo').removeClass('active');
+        $('html,body').animate({ scrollTop: $("#show-trailer").offset().top }, 'slow');
+    }
+
+    $scope.addToFavorites = function() {
+        $('#favoritesBtn').toggleClass('active');
+    }
+
+    $scope.addToWatchlist = function() {
+        $('#watchlistBtn').toggleClass('active');
     }
 }]);
--- public/js/services/SingleShowService.js
+++ public/js/services/SingleShowService.js
@@ -2,6 +2,12 @@ angular.module('SingleShowService', []).factory('SingleShow', ['$http', function
     return {
         getShowData: function(showId) {
             return $http.get('/api/show/' + showId);
+        },
+        addShowToFavorites: function(showId) {
+            return $http.post('/api/show/' + showId + '/addToFav');
+        },
+        addShowToWatchlist: function(showId) {
+            return $http.post('/api/show/' + showId + '/addToFav');
         }
     }
 }]);
