8bb8f12bb4d42cd4027eddc6564987fcfc04ec85
Theodora Nikolova <theodora.nikolova7@gmail.com>
2017-04-27 11:13:54 +0300

    User profile page

--- public/js/controllers/ProfilesCtrl.js
+++ public/js/controllers/ProfilesCtrl.js
@@ -2,4 +2,17 @@ angular.module('ProfilesCtrl', []).controller('ProfilesController', ['$scope', '
     Profiles.getUserProfile().then(function(res) {
         $scope.user = res.data;
     });
+
+     $scope.editProfile = function() {
+        $('#userWatchedList').fadeOut();
+        $('#userFavoriteList').fadeOut();
+        $('#editProfileSection').fadeIn("slow");
+    }
+
+    $scope.cancelEditProfile = function() {
+        $('#editProfileSection').fadeOut();
+        $('#userWatchedList').fadeIn("slow");
+        $('#userFavoriteList').fadeIn("slow");
+        
+    }
 }]);
