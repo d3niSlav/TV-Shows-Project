4348108e4ccac263ace8a0a29adeb44399742d28
Denislav Enchev <deni.b.enchev@gmail.com>
2017-04-29 15:47:47 +0300

    High scores carousel action added.

--- public/js/controllers/RankingCtrl.js
+++ public/js/controllers/RankingCtrl.js
@@ -1,9 +1,19 @@
-angular.module('RankingCtrl', []).controller('RankingController', ['$scope', 'Ranking', function($scope, Ranking) {
+angular.module('RankingCtrl', []).controller('RankingController', ['$scope', '$interval', 'Ranking', function($scope, $interval, Ranking) {
+    var currentShowIndex = 1;
+
     Ranking.getTopShows().then(function(res) {
         $scope.topThreeShows = res.data.slice(0, 3);
+
         $scope.topThreeShows[0].medal = "../images/gold.png";
         $scope.topThreeShows[1].medal = "../images/silver.png";
         $scope.topThreeShows[2].medal = "../images/bronze.png";
+
+        $scope.topThreeShows[0].medalToken = "../images/gold-medal.png"
+        $scope.topThreeShows[1].medalToken = "../images/silver-medal.png"
+        $scope.topThreeShows[2].medalToken = "../images/bronze-medal.png"
+
+        $scope.showOnFokus = $scope.topThreeShows[0];
+        $scope.changeShow = changeShow;
 
         $scope.nextThreeShows = res.data.slice(3, 6);
 
@@ -31,4 +41,30 @@ angular.module('RankingCtrl', []).controller('RankingController', ['$scope', 'Ra
             return stars;
         }
     });
+
+    var changeShow = function(number) {
+        currentShowIndex = number + 1;
+        $scope.showOnFokus = $scope.topThreeShows[number];
+    }
+
+    function getIndex() {
+        if (currentShowIndex > 2) {
+            currentShowIndex = 0;
+        }
+        return currentShowIndex++;
+    }
+
+    startCarousel();
+
+    $scope.pauseCarousel = function() {
+        $interval.cancel(carousel);
+    }
+
+    function startCarousel() {
+        carousel = $interval(function() {
+            changeShow(getIndex());
+        }, 4000);
+    }
+    $scope.startCarousel = startCarousel;
+
 }]);
