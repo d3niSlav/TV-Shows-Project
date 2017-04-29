979437a66d3b413bd8032359f4eaf8b08247b7a7
Denislav Enchev <deni.b.enchev@gmail.com>
2017-04-29 13:11:31 +0300

    In Browse section -> sorting by title, raiting and date. Categories filter added for the  show genres.

--- public/js/controllers/AllShowsCtrl.js
+++ public/js/controllers/AllShowsCtrl.js
@@ -1,6 +1,86 @@
 angular.module('AllShowsCtrl', []).controller('AllShowsController', ['$scope', 'AllShows', function($scope, AllShows) {
+    $scope.orderCriteria = 'title';
 
     AllShows.getAllShows().then(function(res) {
-        $scope.shows = res.data;
+        var shows = res.data
+
+        $scope.shows = shows;
+        $scope.categories = AllShows.getCategories(shows);
+        $scope.categoriesArray = JSON.parse(AllShows.createFiterJSON($scope.categories));
+
+        $scope.categoryFilter = function(a) {
+            var selectedCategories = [];
+
+            for (var index = 0; index < $scope.categoriesArray.length; index++) {
+                var currCategory = $scope.categoriesArray[index];
+                if (currCategory.on) {
+                    selectedCategories.push(currCategory.name);
+                }
+            }
+
+            if (selectedCategories.length) {
+                for (category in $scope.categoriesArray) {
+                    var t = $scope.categoriesArray[category];
+                    if (t.on) {
+                        if (a.genre.indexOf(t.name) < 0) {
+                            return false;
+                        }
+                    }
+                }
+                return true;
+            } else {
+                return true;
+            }
+        };
     });
+
+    function switchCategories() {
+        if ($('.open-categories').hasClass('open')) {
+            closeCategories();
+        } else {
+            openCategories();
+        }
+        $('.open-categories').toggleClass('open');
+    }
+    $scope.switchCategories = switchCategories;
+
+    function closeCategories() {
+        $('.open-categories').toggleClass('hide');
+        $('.close-categories').toggleClass('hide');
+        $('.categories-list').fadeOut();
+    }
+
+    function openCategories() {
+        $('.open-categories').toggleClass('hide');
+        $('.close-categories').toggleClass('hide');
+        $('.categories-list').fadeIn("slow");
+    }
+
+    function orderByTitleAsc() {
+        $('.order-by-title-asc').addClass('active');
+        $('.order-by-title-asc').siblings().removeClass('active');
+        $scope.orderCriteria = 'title';
+    }
+    $scope.orderByTitleAsc = orderByTitleAsc;
+
+    function orderByTitleDesc() {
+        $('.order-by-title-desc').addClass('active');
+        $('.order-by-title-desc').siblings().removeClass('active');
+        $scope.orderCriteria = '-title';
+    }
+    $scope.orderByTitleDesc = orderByTitleDesc;
+
+    function orderByRating() {
+        $('.order-by-rating').addClass('active');
+        $('.order-by-rating').siblings().removeClass('active');
+        $scope.orderCriteria = '-imdbRating';
+    }
+    $scope.orderByRating = orderByRating;
+
+    function orderByDate() {
+        $('.order-by-date').addClass('active');
+        $('.order-by-date').siblings().removeClass('active');
+        $scope.orderCriteria = '-releasedDate';
+    }
+    $scope.orderByDate = orderByDate;
 }]);
--- public/js/services/AllShowsService.js
+++ public/js/services/AllShowsService.js
@@ -6,6 +6,36 @@ angular.module('AllShowsService', []).factory('AllShows', ['$http', function($ht
 
         getAllShowsByDate: function() {
             return $http.get('/api/shows/newest');
+        },
+
+        getCategories: function(shows) {
+            var categories = new Set();
+
+            for (var index = 0; index < shows.length; index++) {
+                var currentShow = shows[index];
+                currentShowCategories = currentShow.genre.split(',');
+
+                for (var catIndex = 0; catIndex < currentShowCategories.length; catIndex++) {
+                    var categorie = currentShowCategories[catIndex];
+                    categories.add(categorie.trim());
+                }
+            }
+
+            return Array.from(categories);
+        },
+
+        createFiterJSON: function(categories) {
+            function Criteria(name) {
+                this.name = name;
+                this.on = false;
+            };
+
+            var result = [];
+            categories.forEach(function(category) {
+                result.push(new Criteria(category));
+            });
+
+            return JSON.stringify(result);
         }
     }
 }]);
